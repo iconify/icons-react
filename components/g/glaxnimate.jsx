9d823b2bd087e7a58f13ccfbe5c533c625f02317
Vjacheslav Trushkin <cyberalien@gmail.com>
2026-09-20 13:40:20 +0300

    chore: update files

--- components/g/glaxnimate.jsx
+++ components/g/glaxnimate.jsx
@@ -1,7 +1,7 @@
 import { Icon } from '@iconify/css-react';
 import { createElement } from 'react';
 import '../../css/e/el5krsbzi.css';
-import '../../css/r/r_xmipeoc.css';
+import '../../css/f/f7i48qbap.css';
 import '../../css/i/i23tqphhu.css';
 import '../../css/e/edxstybzy.css';
 import '../../css/o/or-k32bku.css';
@@ -15,7 +15,7 @@ function Component({width, height, ...props}) {
 		width,
 		height,
 		viewBox,
-		"content": `<g transform="translate(0 -161.533)" class="el5krsbzi"><path class="r_xmipeoc"/><path class="i23tqphhu"/><circle class="edxstybzy"/><circle class="or-k32bku"/></g>`,
+		"content": `<g transform="translate(0 -161.533)" class="el5krsbzi"><path class="f7i48qbap"/><path class="i23tqphhu"/><circle class="edxstybzy"/><circle class="or-k32bku"/></g>`,
 		"fallback": "thesvg-color:glaxnimate",
 	});
 }
